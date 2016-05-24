d666f961d7b8cb079ad670d1906250f985499233
Caroooo <caroline.meusburger@students.fhv.at>
2016-05-24 17:06:54 +0200

    input validation

--- webapp/controller/Register.controller.js
+++ webapp/controller/Register.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
-    'sap/ui/model/json/JSONModel'
+    'sap/ui/model/json/JSONModel',
+    'sap/ui/model/SimpleType',
+    'sap/ui/model/ValidateException'
 
-], function (Controller, JSONModel) {
+], function (Controller, JSONModel, SimpleType, ValidateException) {
     "use strict";
     return Controller.extend("sap.ui.demo.wt.controller.Register", {
 
@@ -19,8 +21,28 @@ sap.ui.define([
             });
             this.getView().setModel(userModel);
             this.config = new sap.ui.core.Configuration();
+
+            // attach handlers for validation errors
+            sap.ui.getCore().attachValidationError(function (evt) {
+                var control = evt.getParameter("element");
+                if (control && control.setValueState) {
+                    control.setValueState("Error");
+                }
+            });
+            sap.ui.getCore().attachValidationSuccess(function (evt) {
+                var control = evt.getParameter("element");
+                if (control && control.setValueState) {
+                    control.setValueState("None");
+                }
+            });
         },
         onSignUp : function(oEvent) {
+
+
+
+
+
+
             var userModel = this.getView().getModel().oData;
             $.ajax({
                 url: "sign-up",
@@ -32,7 +54,24 @@ sap.ui.define([
                 // msg.id is the id of the newly created user
                 this.getOwnerComponent().getRouter().navTo("main");
             });
-        }
+        },
+        typeEMail : SimpleType.extend("email", {
+            formatValue: function (oValue) {
+                return oValue;
+            },
+            parseValue: function (oValue) {
+                //parsing step takes place before validating step, value can be altered
+                return oValue;
+            },
+            validateValue: function (oValue) {
+                // The following Regex is NOT a completely correct one and only used for demonstration purposes.
+                // RFC 5322 cannot even checked by a Regex and the Regex for RFC 822 is very long and complex.
+                var mailregex = '\\S+@\\S+\\.\\S+';
+                if (!oValue.match(mailregex)) {
+                    throw new ValidateException("'" + oValue + "' is not a valid email address");
+                }
+            }
+        })
 
     });
 });
